0731b5b7fce551c2b54ccd5e0ac7547d8f09a890
bkhorvat <bkhorvat.php@gmail.com>
2022-12-30 06:00:15 +0100

    Add atributes for form

--- src/Form/EditProductFormType.php
+++ src/Form/EditProductFormType.php
@@ -4,16 +4,29 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EditProductFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('Price')
+            ->add('title', TextType::class, [
+                'label' => 'Title (from class)',
+                'required' => true
+            ])
+            ->add('Price', NumberType::class, [
+                'label' => 'Title (from class)',
+                'scale' => 2,
+                'html5' => true,
+                'attr' => [
+                    'step' => '0.01'
+                ]
+            ])
             ->add('quantity')
             ->add('description')
             ->add('isPublished')
